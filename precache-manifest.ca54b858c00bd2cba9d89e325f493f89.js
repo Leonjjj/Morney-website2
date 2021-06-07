444fbdad35b3bb5cba9df643a61cbf1376a24717
Leon <410763533@qq.com>
2021-06-07 20:29:56 +0800

    deploy

---
+++ precache-manifest.ca54b858c00bd2cba9d89e325f493f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ede1144d09ae6ed8ce",
+    "url": "/Morney-website2/css/app.e13d33a5.css"
+  },
+  {
+    "revision": "2564f86c1226c1534be62b5c7af3cc3c",
+    "url": "/Morney-website2/index.html"
+  },
+  {
+    "revision": "20ede1144d09ae6ed8ce",
+    "url": "/Morney-website2/js/app.a78fb2c8.js"
+  },
+  {
+    "revision": "4a0f7dd61a25118670c3",
+    "url": "/Morney-website2/js/chunk-vendors.91ef844e.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Morney-website2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website2/robots.txt"
+  }
+]);
